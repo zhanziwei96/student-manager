ddeec52cc1b4f593fa7b04a48e155ac584a0f546
Developer <developer@example.com>
2026-04-13 23:22:32 +0800

    feat(frontend): add useCloneGroupTask composable

--- frontend-v3/src/features/group-collaboration/index.ts
+++ frontend-v3/src/features/group-collaboration/index.ts
@@ -1,4 +1,4 @@
-export { useTeacherGroupTasks, useCreateGroupTask, useStartGroupTask, useCloseGroupTask, useTaskResults, useSubmitTeacherScore } from './composables/useGroupTasks'
+export { useTeacherGroupTasks, useCreateGroupTask, useStartGroupTask, useCloseGroupTask, useTaskResults, useSubmitTeacherScore, useCloneGroupTask } from './composables/useGroupTasks'
 export { useTeacherGroups, useAutoAssign, useStudentGroups, useMyGroup, useCreateGroup, useJoinGroup, useApproveJoin } from './composables/useGroups'
 export { useStudentGroupTasks, useEvaluations, useSubmitStudentScores, useMyGroupResults } from './composables/useEvaluations'
 export { useClassGroupSettings, useUpdateClassGroupSettings } from './composables/useGroupSettings'
